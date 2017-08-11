6e73fa60e589f2387ff438e02fb0efe66d60b5df
Casey Youngflesh <caseyyoungflesh@gmail.com>
2017-08-11 18:04:52 -0400

    Add Kerry and Schaub 2012 model

---
+++ Scripts/Kerry_and_schaub_2012_model.R
@@ -0,0 +1,11 @@
+#CH 7  Kerry and Schaub 2012
+
+#define param values
+n.accasions <- 6
+marked <- rep(50, n.occasions - 1)
+
+phi <- rep(0.65, n.accasions - 1)
+p <- rep(0.4, n.accasions - 1)
+
+#define matrixes and survival and recapture probs
+PHI
