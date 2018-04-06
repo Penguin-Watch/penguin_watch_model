8178bcc56cf51ee51e746817a944ced03494a297
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-04-05 20:14:42 -0400

    finish test processing

--- Scripts/1-process-data.R
+++ Scripts/1-process-data.R
@@ -1,7 +1,7 @@
 #################
 # Penguin Watch Model - 1 - Process data
 #
-# 1 - process data
+# 1 - process data (krill data)
 # 1 - simulate data
 # 2 - run model
 # 3 - analyze output
@@ -30,11 +30,6 @@ pacman::p_load(dplyr, rgdal, rgeos)
 # setwd -------------------------------------------------------------------
 
 setwd('Data/Site_data/')
-
-
-# penguin data -----------------------------------------------------------
-
-
 
 
 # create buffers ----------------------------------------------------------
@@ -73,7 +68,7 @@ points(tnp, col = 'red', pch = 19)
 #----------------------------#
 
 
-
+setwd('../Krill_data')
 
 
 #sites with processed penguin watch data:
@@ -81,20 +76,44 @@ cam_sites <- c('AITC', 'BAIL', 'BOOT', 'CUVE', 'DAMO', 'DANC', 'GEOR',
                'HALF', 'MAIV', 'NEKO', 'PETE', 'SPIG', 'SSIS')
 
 
+test <- read.csv('test.csv')
+
+#lat/lons
+pts <- data.frame(Longitude = test$startLon, 
+                     Latitude = test$startLat)
+
+#points are in 4326 (uses lat/lon)
+spatial_pts <- SpatialPoints(pts, proj4string = CRS("+init=epsg:4326"))
+#transform to 3031 (to match everything else)
+tr_spatial_pts <- spTransform(spatial_pts, CRS(proj4string(sites)))
+
+
 #to select individual sites
+
+master_krill <- data.frame()
 
 for (i in 1:length(cam_sites))
 {
   i <- 1
   temp_site <- subset(sites, site_id == cam_sites[i])
+  #transform to degrees
+  #spTransform(temp_site, CRS("+init=epsg:4326"))
   
   #150km (150,000m)
   temp_buffer <- rgeos::gBuffer(temp_site, width = 150000)
+  
+  #which trawls started within colony i buffer
+  temp_in_buff <- which(as.numeric(over(tr_spatial_pts, temp_buffer)) == 1)
+  temp_trawls <- mutate(test[temp_in_buff,], col_id = cam_sites[i])
+  master_krill <- rbind(master_krill, temp_trawls)
+
+  #---------------#  
+  #plot everything
   #plot(site_buffer)
-  #points(nsite, pch = 19, col = 'red')
+  #points(temp_site, pch = 19, col = 'red')
   #plot(temp_buffer, add = TRUE)
-  
-  
+  #points(tr_spatial_pts, pch = '+', col = 'green')
+  #---------------#
 }
 
 
