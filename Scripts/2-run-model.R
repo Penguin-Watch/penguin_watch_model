404cfa0f30b29199f26ca78c802f938c0e7d7fb3
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-04-17 17:44:02 -0400

    Restructure loops of code and response array

--- Scripts/2-run-model.R
+++ Scripts/2-run-model.R
@@ -51,12 +51,14 @@ require(abind)
 
 # determine PW dates to use -----------------------------------------------
 
-
+#ensures that row dimension (time steps within season) will have the same dimension
 
 #remove HALF bc it's not a full season
 un_sites_p <- unique(PW_data$site)
 un_sites <- un_sites_p[which(un_sites_p != 'HALF')]
 
+#first date of each season
+yrs <- c()
 min_date <- as.Date(NA)
 for (k in 1:length(un_sites))
 {
@@ -64,11 +66,12 @@ for (k in 1:length(un_sites))
   temp <- filter(PW_data, site == un_sites[k])
   un_yrs <- unique(temp$season_year)
   
+  yrs <- c(yrs, un_yrs)
   for (j in 1:length(un_yrs))
   {
     #j <- 1
     temp2 <- filter(temp, season_year == un_yrs[j])
-    
+
     temp_dates <- as.Date(temp2$datetime, format = "%Y:%m:%d %H:%M:%S")
     t_min_date <- min(temp_dates)
     min_date <- c(min_date, t_min_date)
@@ -76,11 +79,11 @@ for (k in 1:length(un_sites))
 }
 
 
-#first date of season to use (add 1 to start on full day)
+#first date of season to use across all years (add 1 to start on full day)
 f_min_date <- format((min_date+1), '%m-%d')
 first_date <- max(f_min_date, na.rm = TRUE)
 
-#use Feb 1 as last date of season to use
+#use Feb 1 as last date of season to use across all years
 last_date <- format(as.Date('02-01', format = '%m-%d'), '%m-%d')
 
 
@@ -101,88 +104,102 @@ to.rm <- c(to.rm1, to.rm2)
 tog <- c(ind, to.rm)
 tog2 <- tog[!(duplicated(tog) | duplicated(tog, fromLast = TRUE))]
 
-PW_nests <- PW_data[,tog2]
+
+#number of time steps (rows) in response data (-1 to account for greater than FIRST and less than LAST) - t
+n_ts <- as.numeric(LAST - FIRST - 1) * 24
+
+#number of nests (columns) in response data - i
+n_nests <- length(tog2)
+
+#number of years (3rd dim) in response data - j
+d_yrs <- sort(yrs[!duplicated(yrs)])
+n_yrs <- length(d_yrs)
+
+#number of sites (4th dim) in response data - k
+n_sites <- length(un_sites)
+
+#create blank array
+nests_array <- array(NA, dim = c(n_ts, n_nests, n_yrs, n_sites))
+w_array <- array(NA, dim = c(n_ts, n_nests, n_yrs, n_sites))
+z_array <- array(NA, dim = c(n_ts, n_nests, n_yrs, n_sites))
 
 
-dims <- c()
-for (k in 1:length(un_sites))
+
+#fill array
+for (k in 1:n_sites)
 {
-  #k <- 5
+  #k <- 1
   temp <- filter(PW_data, site == un_sites[k])
-  un_yrs <- unique(temp$season_year)
   
-  for (j in 1:length(un_yrs))
+  for (j in 1:n_yrs)
   {
     #j <- 2
-    temp2 <- filter(temp, season_year == un_yrs[j])
-    temp_dates <- as.Date(temp2$datetime, format = "%Y:%m:%d %H:%M:%S")
+    temp2 <- filter(temp, season_year == d_yrs[j])
+    if (NROW(temp2) > 0)
+    {
+      temp_dates <- as.Date(temp2$datetime, format = "%Y:%m:%d %H:%M:%S")
     
-    #first and last days in season
-    FIRST <- as.Date(paste0((un_yrs[j]-1), '-', first_date), format = "%Y-%m-%d")
-    LAST <- as.Date(paste0(un_yrs[j], '-', last_date), format = "%Y-%m-%d")
+      #first and last days in season
+      FIRST <- as.Date(paste0((d_yrs[j]-1), '-', first_date), format = "%Y-%m-%d")
+      LAST <- as.Date(paste0(d_yrs[j], '-', last_date), format = "%Y-%m-%d")
+      valid_dates <- which(temp_dates > FIRST & temp_dates < LAST)
     
-    valid_dates <- which(temp_dates > FIRST & temp_dates < LAST)
-    
-    #appropriate date range and appropriate columns for nests
-    temp_nests <- temp2[valid_dates, tog2]
-    dims <- rbind(dims, dim(temp_nests))
+      #appropriate date range and appropriate columns for nests
+      nests_array[,,j,k] <- as.matrix(temp2[valid_dates, tog2])
+    }
   }
 }
 
-dims
 
+#rows are years, columns are sites
+real_nests <- matrix(NA, nrow = n_yrs, ncol = n_sites)
+for (k in 1:dim(nests_array)[4])
+{
+  #k <- 1
+  for (j in 1:dim(nests_array)[3])
+  {
+    #j <- 2
+    #13 rows to make sure NA are due to night 
+    temp <- nests_array[1:13,,j,k]
+    
+    tsum <- c()
+    for (m in 1:13)
+    {
+      #m <- 7
+      tsum <- c(tsum, sum(!is.na(temp[m,])))
+    }
+    #max is going to be number of nests in image
+    real_nests[j,k] <- max(tsum)
+  }
+}
 
-
-
-
-
-max_rows <- max(dims[,1])
 
 # Create PW array ---------------------------------------------------------
 
+#nests_array:
+#dim1 (rows) [t] = time steps
+#dim2 (cols) [i] = nests
+#dim3 [j] = years
+#dim4 [k] = sites
 
-nests_array <- c()
-un_sites <- unique(PW_data$site)
-for (k in 1:length(un_sites))
-{
-  #k <- 2
-  temp <- filter(PW_data, site == un_sites[k])
-  un_yrs <- unique(temp$season_year)
-  
-  years_array <- c()
-  for (j in 1:length(un_yrs))
-  {
-    #j <- 1
-    temp2 <- filter(temp, season_year == un_yrs[j])
-    temp_nests <- temp2[,tog2]
-    years_array <- abind(years_array, temp_nests, along = 3)
-  }
-  
-  nests_array <- abind(nests_array, years_array, along = 4)
-}
-
-
-
-
-
-
-#structure into 4 dimensional array ([X,,,] = i (nest_id), [,X,,] = t (time_step), 
-#[,,X,] = j (year), [,,,X] = k (site))
-
+#need array that has nights 
+#need array that has z vals
 
 
 
 DATA <- list(
-  y = sim_data, #reponse
-  N = NROW(sim_data), #number of nests
-  L = NCOL(sim_data), #number of time points
-  z = z_vals,
-  x = 1:NCOL(sim_data),
-  w = w_mat) #binary day (1)/night (0)
+  y = nests_array, #reponse
+  NK = dim(nests_array)[4], #number of sites
+  NJ = dim(nests_array)[3], #number of years covered for all sites
+  NI = real_nests, #matrix with number of nests for each site/year NI[j,k]
+  NT = dim(nests_array)[1], #number of time steps
+  z = z_array, #known points of bird being alive
+  w = w_array) #binary day (1)/night (0)
 
 
 
 # Model -------------------------------------------------------------------
+
 
 #L = length of time series (400)
 #N = number of nests (5)
@@ -193,56 +210,78 @@ sink("pwatch_surv.jags")
 cat("
     model {
   
-    for (i in 1:N)
+
+#sites
+for (k in 1:NK)
+{
+  #years
+  for (j in 1:NJ)
+  {
+    #nests
+    for (i in 1:NI[j,k])
     {
-    #both chicks alive at time step 1
-    z[i,1] <- 2
-    y.new[i,1] ~ dbinom(p[i,1], 2)
+      #both chicks alive at time step 1
+      z[1,i,j,k] <- 2
+      #y.new[1,i,j,k] ~ dbinom(p[1,i,j,k], 2)
+
+
+      #time step
+      for (t in 2:NT)
+      {
+        #state model
+        z[t,i,j,k] ~ dbinom(p_alive[t,i,j,k], z[t-1,i,j,k])
+        p_alive[t,i,j,k] <- ifelse(z[t-1,i,j,k] < 2, 
+                                phi[t,i,j,k] * z[t-1,i,j,k],
+                                phi[t,i,j,k])
     
-    for (t in 2:L)
+        #observation model
+        y[t,i,j,k] ~ dbinom(p_sight[t,i,j,k] * w[t,i,j,k], z[t,i,j,k]) #binary day/night
+        p_sight[t,i,j,k] <- ifelse(z[t,i,j,k] < 2,
+                                p[t,i,j,k] * z[t,i,j,k],
+                                p[t,i,j,k])
+    
+        #PPC
+        #y.new[t,i,j,k] ~ dbinom(p_sight[t,i,j,k] * w[t,i,j,k], z[t,i,j,k]) #binary day/night
+      }
+    }
+  }
+}
+    
+    
+
+#PPC
+#mean
+#mn.y <- mean(y)
+#mn.y.new <- mean(y.new)
+#pv.mn <- step(mn.y.new - mn.y)
+    
+#sd
+#sd.y <- sd(y)
+#sd.y.new <- sd(y.new)
+#pv.sd <- step(sd.y.new - sd.y)
+    
+    
+#transforms
+#sites
+for (k in 1:NK)
+{
+  #years
+  for (j in 1:NJ)
+  {
+    #nests
+    for (i in 1:NI[j,k])
     {
-    
-    #state model
-    z[i,t] ~ dbinom(p_alive[i,t], z[i,t-1])
-    p_alive[i,t] <- ifelse(z[i,t-1] < 2, 
-                        phi[i,t] * z[i,t-1],
-                        phi[i,t])
-    
-    #observation model
-    y[i,t] ~ dbinom(p_sight[i,t] * w[i,t], z[i,t]) #binary day/night
-    p_sight[i,t] <- ifelse(z[i,t] < 2,
-                        p[i,t] * z[i,t],
-                        p[i,t])
-    
-    
-    #PPC
-    y.new[i,t] ~ dbinom(p_sight[i,t] * w[i,t], z[i,t]) #binary day/night
+      logit(phi[t,i,j,k]) <- mu_phi + beta_phi*x[t] + eps_phi[t]       #phi = survival prob
+      logit(p[t,i,j,k]) <- mu_p + beta_p*x[t] + eps_p[i]            #p = detection prob
     }
-    }
+  }
     
-    #PPC
-    #mean
-    mn.y <- mean(y)
-    mn.y.new <- mean(y.new)
-    pv.mn <- step(mn.y.new - mn.y)
-    
-    #sd
-    sd.y <- sd(y)
-    sd.y.new <- sd(y.new)
-    pv.sd <- step(sd.y.new - sd.y)
-    
-    
-    #transforms
-    for (i in 1:N)
-    {
-    for (t in 1:L)
-    {
-    logit(phi[i,t]) <- mu_phi + beta_phi*x[t] + eps_phi[t]       #phi = survival prob
-    logit(p[i,t]) <- mu_p + beta_p*x[t] + eps_p[i]            #p = detection prob
-    }
-    }
-    
-    
+
+
+
+
+
+
     #priors
     for (t in 1:L)
     {
