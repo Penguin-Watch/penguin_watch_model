4c9aeae4b32f1f066420df4695aee47ea6594d41
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-07-17 00:17:51 -0400

    pre HPC

--- Scripts/2-model.R
+++ Scripts/2-model.R
@@ -16,13 +16,13 @@ rm(list = ls())
 # DIR ---------------------------------------------------------------------
 
 #laptop
-dir <- c('~/Google_Drive/R/penguin_watch_model/Data/PW_data/',
-         '../Krill_data/CCAMLR/Processed_CCAMLR/',
-         '../../../SIC_data/Processed/',
-         '~/Google_Drive/R/penguin_watch_model/Results/')
+# dir <- c('~/Google_Drive/R/penguin_watch_model/Data/PW_data/',
+#          '../Krill_data/CCAMLR/Processed_CCAMLR/',
+#          '../../../SIC_data/Processed/',
+#          '~/Google_Drive/R/penguin_watch_model/Results/')
 
 #HPC
-# dir <- c('../Data', '../Data', '../Data', '../Results')
+dir <- c('../Data', '../Data', '../Data', '../Results')
 
 
 
