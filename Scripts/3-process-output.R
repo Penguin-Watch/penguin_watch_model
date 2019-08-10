900aba540d062814217c5fb1b52d774afc87676a
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-08-10 00:00:13 -0400

    save other objs

--- Scripts/3-process-output.R
+++ Scripts/3-process-output.R
@@ -333,3 +333,16 @@ mrg6$YEAR <- as.numeric(mrg6$YEAR)
 saveRDS(mrg6, 'bs_precip_mrg.rds')
 
 
+
+
+# save z_out and p_out ----------------------------------------------------
+
+setwd(OUTPUT)
+
+z_out_obj <- MCMCvis::MCMCchains(fit, params = 'z_out', mcmc.list = TRUE)
+p_out_obj <- MCMCvis::MCMCchains(fit, params = 'p_out', mcmc.list = TRUE)
+
+saveRDS(z_out_obj, 'z_out.rds')
+saveRDS(p_out_obj, 'p_out.rds')
+saveRDS(data, 'jagsData.rds')
+saveRDS(precip_df, 'precip_df.rds')
