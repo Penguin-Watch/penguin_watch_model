9e0dae50796f25da7479857dbb80919202eceb0b
Casey Youngflesh <caseyyoungflesh@gmail.com>
2019-09-08 14:50:12 -0400

    process tourism data

--- Scripts/5-process-tourism-data.R
+++ Scripts/5-process-tourism-data.R
@@ -34,7 +34,9 @@ library(sp)
 
 setwd(OUTPUT)
 
-mrg <- readRDS('bs_precip_krill_mrg.rds')
+mrg_p <- readRDS('bs_precip_krill_mrg.rds')
+
+mrg <- dplyr::filter(mrg_p, SOURCE == 'PW')
 
 #read in model input data
 data <- readRDS('jagsData.rds')
@@ -46,18 +48,23 @@ start <- as.numeric(format(as.Date(as.numeric(julian(mrg$creche_date)) - 59,
 
 # read in tourism data ----------------------------------------------------
 
-setwd(paste0(dir, 'Data/tourism_data/IAATO_site_visits_reports'))
+setwd(paste0(dir, 'Data/tourism_data/IAATO_site_visits_reports-2019-09-02/csv'))
 
 fls <- list.files()
 
 #get dates for each site/year (lay to fledge)
 #sum total visitors for each site/year based on those dates
 #plot # of visitors each day over course of season
+t_df <- data.frame(SITE = rep(NA, length(fls)), YEAR = NA, t_visitors = NA)
 for (i in 1:length(fls))
 {
-  #i <- 3
+  #i <- 4
   temp <- read.csv(fls[i])
-  temp$total_visiting
+  
+  nm <- strsplit(fls[i], '_')[[1]]
+  t_df$SITE[i] <- nm[1]
+  t_df$YEAR[i] <- as.numeric(strsplit(nm[2], split = '.', fixed = TRUE)[[1]][1])
+  t_df$t_visitors[i] <- sum(temp$total_visiting, na.rm = TRUE)
 }
 
 
@@ -66,6 +73,8 @@ for (i in 1:length(fls))
 
 setwd(OUTPUT)
 
+mrg2 <- left_join(mrg, t_df, by = c('SITE', 'YEAR'))
+
 #save as rds
-saveRDS(XXXX, 'master_output.rds')
+saveRDS(mrg2, 'master_output.rds')
 
